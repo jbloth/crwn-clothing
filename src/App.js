de6ac0742941f1bcaad71f763485ad73dcacb72a
Juliane <julianewonka@Julianes-MBP.fritz.box>
2020-04-02 20:32:15 +0200

    added checkout item. Got cart-dropdown to hide autmoatically on cart page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-
+import { createStructuredSelector } from 'reselect';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
 
 class App extends React.Component {
   unsuscribeFromAuth = null;
@@ -58,6 +61,7 @@ class App extends React.Component {
             path="/signin"
             render={() => (this.props.currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />)}
           />
+          <Route exact path="/checkout" component={CheckoutPage} />
         </Switch>
       </div>
     );
@@ -66,8 +70,8 @@ class App extends React.Component {
 
 // Brauchen wir um im router einen redirect einzurichten falls eingeloggter user ist
 // die signin-route aufrufen will
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 });
 
 // Wird gebraucht, damit Komponent actions dispatchen kann?
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 // Higher order component that gives us access to redux-reducers (?)
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 // ReactCompnent packt svg automatisch in Component
 import { ReactComponent as Logo } from '../../assets/crown.svg';
@@ -38,9 +43,9 @@ const Header = ({ currentUser, hidden }) => (
 );
 
 // parameter state ist root reducer, wird hier in zwei leveln destructured
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
-  currentUser,
-  hidden
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden
 });
 
 // Gibt Header Komponente Zugriff zu Funktion die Zugriff zu einem Reducer hat.
