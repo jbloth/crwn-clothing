aba29edeb88fc23fb120f86a78912e50c9fd40c3
Juliane <julianewonka@Julianes-MBP.fritz.box>
2020-04-17 10:08:31 +0200

    Updated App-, sign-in, sign-up and shop-component to use hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -16,40 +16,31 @@ import // auth,
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions';
 
-class App extends React.Component {
-  unsuscribeFromAuth = null;
-
-  componentDidMount() {
-    const { checkUserSession } = this.props;
+// checkUserSession ist ein property und könnte sich im Programmverlauf ändern, deswegen
+// sollten wir in useEffect darauf reagieren und deswegen ist checkUserSession der zweite
+// Parameter. Da sich checkUserSession aber wahrscheinlich nicht ändert, verhält sich dieses
+// useEffect quasi wie componentDidMount.
+const App = ({ checkUserSession, currentUser }) => {
+  useEffect(() => {
     checkUserSession();
-  }
+  }, [checkUserSession]);
 
-  // Entfernt den Observer (Denn sonst sammelt der Garbage-Collector die unmountete
-  // Komponente nicht ein?). Hier nicht zwingend nötig, weil wir App Komponente
-  // wahrscheinlich nur ein mal verwenden und unmount bedeutet dass die Seite verlassen
-  // wird, aber generell schon wichtig.
-  // componentWillUnmount() {
-  //   this.unsuscribeFromAuth();
-  // }
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/shop" component={ShopPage} />
-          <Route
-            exact
-            path="/signin"
-            render={() => (this.props.currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />)}
-          />
-          <Route exact path="/checkout" component={CheckoutPage} />
-        </Switch>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route
+          exact
+          path="/signin"
+          render={() => (currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />)}
+        />
+        <Route exact path="/checkout" component={CheckoutPage} />
+      </Switch>
+    </div>
+  );
+};
 
 // Brauchen wir um im router einen redirect einzurichten falls eingeloggter user ist
 // die signin-route aufrufen will
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -10,22 +10,18 @@ import CollectionPageContainer from '../collection/collection.container';
 import './shop.styles.scss';
 
 // Wir haben Zugriff auf match, weil ShopPage als route-ziel für /shop definiert ist.
-class ShopPage extends React.Component {
-  componentDidMount() {
-    const { fetchCollectionsStart } = this.props;
+const ShopPage = ({ match, fetchCollectionsStart }) => {
+  useEffect(() => {
     fetchCollectionsStart();
-  }
+  }, [fetchCollectionsStart]);
 
-  render() {
-    const { match } = this.props;
-    return (
-      <div className="shop-page">
-        <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
-        <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="shop-page">
+      <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
+    </div>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => ({
   fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
